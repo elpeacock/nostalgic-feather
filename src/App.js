8ea4239f96a137cc94e09222d498a6948d7fd971
Liz Peacock <liz.peacock11@gmail.com>
2018-08-28 22:13:55 -0500

    tutorial lists and conditionals

--- src/App.js
+++ src/App.js
@@ -6,35 +6,57 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: 'Liz', age: '35' },
-      { name: 'Sarah', age: '39' }, 
-      { name: 'Max', age: '28' }
+      { id: 'kladsjf', name: 'Liz', age: '35' },
+      { id: 'ioqwefj', name: 'Sarah', age: '39' }, 
+      { id: 'xncvzxk', name: 'Max', age: '28' }
     ], 
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('clickity click click clickeroo...');
-    // DON'T DO THIS: this.state.persons[0].name = 'Elizabeth';
-    // DO THIS INSTEAD:
-    this.setState({
-      persons: [
-        { name: newName, age: '35' },
-        { name: 'Sarah', age: '40' },
-        { name: 'Max', age: '28' }
-      ]
-    })
-  }
+  // switchNameHandler = (newName) => {
+  //   // console.log('clickity click click clickeroo...');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Elizabeth';
+  //   // DO THIS INSTEAD:
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: '35' },
+  //       { name: 'Sarah', age: '40' },
+  //       { name: 'Max', age: '28' }
+  //     ]
+  //   })
+  // }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    // holds index where persons.id === id returns true
+    const changeIndex = this.state.persons.findIndex( p => { 
+      return p.id === id;
+    });
+    // //mutable change == BAD
+    // const changePerson = this.state.persons[changeIndex]
+    // immutable change == GOOD
+    const changePerson = { 
+      ...this.state.persons[changeIndex]
+    };
+
+    changePerson.name = event.target.value;
+
+    const personsMod = [...this.state.persons];
+    personsMod[changeIndex] = changePerson;
+
     this.setState({
-      persons: [
-        { name: 'liz', age: '35' },
-        { name: 'Sarah', age: '40' },
-        { name: event.target.value, age: '28' }
-      ]
+      persons: personsMod
     })
   } 
+
+  deletePersonHandler = (personIndex) => { 
+    // // slice with no args makes a copy of the array
+    // const persons = this.state.persons.slice();
+    // [...] spread operator, will act like slice in this situation
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+    // making the copy of the original array then doing stuff to it and then assigning copy to original makes the array immutable ---- state should always be set immutably!!!
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -55,17 +77,14 @@ class App extends Component {
     if( this.state.showPersons ) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age} />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'Lizzie')}
-            changed={this.nameChangedHandler} >I am the React instructor</Person>
+          { this.state.persons.map((person, index) => { 
+            return <Person
+              click={ this.deletePersonHandler }
+              name={ person.name }
+              age={ person.age }
+              key={ person.id }
+              changed={ (event) => this.nameChangedHandler(event, person.id) } />
+          })}
         </div>
       )
     }
